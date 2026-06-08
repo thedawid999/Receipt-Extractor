496691d56ce338267277fda6d3872bd73d6b0c24
thedawid999 <dawid.jedlinski@iu-study.org>
2026-06-08 08:02:39 +0200

    updated api.py with save_upload()

--- app/api.py
+++ app/api.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI, UploadFile, File
 from pipeline_processor import process_receipt
+from utils import save_upload
 
 # for one file only
 @app.post("/extract")
 async def extract(file: UploadFile = File(...)):
-    return process_receipt(file)
+    file_path = save_upload(file)
+    return process_receipt(file_path)
 
 # for multiple files
 @app.post("/extract-batch")
 async def extract_batch(files: List[UploadFile] = File(...)):
-    return [process_receipt(file) for file in files]
+    results = []
+
+    for file in files:
+        file_path = save_upload(file)
+        results.append(process_receipt(file_path))
+    
+    return results
