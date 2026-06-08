04d390a78e674c6b143b3c8443fb72eb7ae68f15
thedawid999 <dawid.jedlinski@iu-study.org>
2026-06-08 07:47:15 +0200

    defined process_input()

--- app/pipeline_processor.py
+++ app/pipeline_processor.py
@@ -3,10 +3,21 @@ import os
 # used only for local main.py
 # check if path includes only one file or mutliple files
 def process_input(path):
-    # read file
-    # 1 file OR multiplefiles
+    if os.path.isFile:
+        return process_receipt(path)
+    elif os.path.isdir(path):
+        results = []
 
-def process_receipt():
+        for file in os.listdir(path)
+            full_path = os.path.join(path, file)
+
+            if os.file.isFile(full_path) AND file.lower().endswith(".jpg", ".jpeg", ".png"):
+                results.append(process_receipt(full_path))
+        return results
+    else:
+        raise ValueError("Input must be a directory or a file of type .jpg, .jpeg or .png")
+
+def process_receipt(path: str):
     # preprocess
     # ocr 
     # regex
