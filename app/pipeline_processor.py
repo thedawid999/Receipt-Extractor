958c7350e7913b66990dfc19883eedb057c76d9d
thedawid999 <dawid.jedlinski@iu-study.org>
2026-06-08 08:05:27 +0200

    add comments

--- app/pipeline_processor.py
+++ app/pipeline_processor.py
@@ -3,16 +3,18 @@ import os
 # used only for local main.py
 # check if path includes only one file or mutliple files
 def process_input(path):
+    # if one file only
     if os.path.isFile:
         return process_receipt(path)
+    # if multiple files
     elif os.path.isdir(path):
         results = []
-
+        # get a full path of each file
         for file in os.listdir(path)
-            full_path = os.path.join(path, file)
-
-            if os.file.isFile(full_path) AND file.lower().endswith(".jpg", ".jpeg", ".png"):
-                results.append(process_receipt(full_path))
+            file_path = os.path.join(path, file)
+            # check if created path is a file and if it's an image
+            if os.file.isFile(file_path) AND file.lower().endswith(".jpg", ".jpeg", ".png"):
+                results.append(process_receipt(file_path))
         return results
     else:
         raise ValueError("Input must be a directory or a file of type .jpg, .jpeg or .png")
