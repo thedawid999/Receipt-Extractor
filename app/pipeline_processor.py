d744a3f1b64b3a14603c95f287e4c5ec3228d8cc
thedawid999 <dawid.jedlinski@iu-study.org>
2026-06-09 10:03:11 +0200

    resolved syntax errors

--- app/pipeline_processor.py
+++ app/pipeline_processor.py
@@ -10,16 +10,17 @@ def process_input(path):
     elif os.path.isdir(path):
         results = []
         # get a full path of each file
-        for file in os.listdir(path)
+        for file in os.listdir(path):
             file_path = os.path.join(path, file)
             # check if created path is a file and if it's an image
-            if os.file.isFile(file_path) AND file.lower().endswith(".jpg", ".jpeg", ".png"):
+            if os.file.isFile(file_path) and file.lower().endswith(".jpg", ".jpeg", ".png"):
                 results.append(process_receipt(file_path))
         return results
     else:
         raise ValueError("Input must be a directory or a file of type .jpg, .jpeg or .png")
 
 def process_receipt(path: str):
+    return None
     # preprocess
     # ocr 
     # regex
