7d74e0a12aed70af6fc175f50dc2fc38a5b70cf5
thedawid999 <dawid.jedlinski@iu-study.org>
2026-06-14 11:13:19 +0200

    added grayscale again and rotation for skewed pictures

--- app/preprocessing.py
+++ app/preprocessing.py
@@ -1,38 +1,25 @@
-import cv2 as cv2
+import cv2
 import numpy as np 
-from matplotlib import pyplot as plt 
-import os
+from deskew import determine_skew
 
+# adds grayscale and rotates the image if skewed
 def preprocess(path: str) -> np.ndarray:
     img = cv2.imread(path)
 
     # add grayscale (black-and-white)
-    #grayed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # remove noise
-    #denoised = cv2.bilateralFilter(grayed, d=3, sigmaColor=40, sigmaSpace=40)
-    # binarise (increases contrast additionaly)
-    #binarised = cv2.adaptiveThreshold(denoised, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 6)
+    grayed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # rotate
+    angle = determine_skew(grayed)
+    rotated = rotate(angle, grayed)
 
-    return img
+    return rotated
 
-    
+def rotate(angle:float, img: np.ndarray):
+    (h, w) = img.shape
+    center = (w // 2, h // 2)
 
-    # CREATED TO FIND BEST PARAMETER VALUES FOR PREPROCESSING
-    #path = "samples/original"
-    #new_path = "samples/preprocessed"
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
 
-    #titles = ["original", "grayscaled", "denoised", "binarised"]
-    #images = [img_rgb, grayed, denoised, binarised]
+    rotated = cv2.warpAffine(img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
 
-    #plt.figure(figsize=(8,18))
-    #for i in range(4):
-    #    plt.subplot(2,5,i+1)
-    #    if i == 0:
-    #        plt.imshow(images[i])
-    #    else:
-    #        plt.imshow(images[i], cmap="gray")
-    #    plt.title(titles[i])
-    #    plt.axis("off")
-
-    #    new_filename = os.path.join(new_path, file)
-    #plt.savefig(new_filename, dpi=300, bbox_inches="tight")
+    return rotated
