4fb070cb606d51a0120afff53a2b3c8924986aec
thedawid999 <dawid.jedlinski@iu-study.org>
2026-06-14 15:32:04 +0200

    adjusted detect_text() with two new methods from utils.py

--- app/ocr.py
+++ app/ocr.py
@@ -1,18 +1,22 @@
 import easyocr
 import numpy as np
-from preprocessing import preprocess
+from utils import convert_to_topleft_and_bottomright, standardize_bbox
 
 reader = easyocr.Reader(["en"])
 
 def detect_text(img: np.ndarray):
     results = reader.readtext(img)
+    height, width = img.shape[:2]
 
     output = []
     for bbox, text, conf in results:
         if conf > 0.4:
+            converted = convert_to_topleft_and_bottomright(bbox)
+            standardized = standardize_bbox(converted, height, width)
+
             output.append(
                 {
-                    "bbox": [[int(x), int(y)] for x, y in bbox],
+                    "bbox": standardized,
                     "text": text,
                 }
             )
