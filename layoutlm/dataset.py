47e69c6e79d77620502277a96b249bdf4aa587f2
thedawid999 <dawid.jedlinski@iu-study.org>
2026-06-22 18:27:21 +0200

    defined assign_labels()

--- layoutlm/dataset.py
+++ layoutlm/dataset.py
@@ -4,6 +4,10 @@ import numpy as np
 import pandas as pd
 import json
 
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 2000)
+
 base = kagglehub.dataset_download("urbikn/sroie-datasetv2")
 base = Path(base)
 sroie_path = base / "SROIE2019"
@@ -11,8 +15,8 @@ sroie_path = base / "SROIE2019"
 # reads box-file containing bbox coordinates and the word inside
 # leaves only top-left and bottom-right bbox coordinates
 # returns a DataFrame
-def read_bbox(path: Path):
-    bbox_list = []
+def read_bboxes(path: Path):
+    bboxes_list = []
 
     with open(path, "r") as f:
         for line in f.read().splitlines():
@@ -24,9 +28,9 @@ def read_bbox(path: Path):
             bbox = np.array(lines[0:8], dtype = np.int32)
             text = ",".join(lines[8:])
 
-            bbox_list.append([path.stem, *bbox, text])
+            bboxes_list.append([path.stem, *bbox, text])
 
-        dataframe = pd.DataFrame(bbox_list, columns=['filename', 'x0', 'y0', 'x1', 'y1', 
+        dataframe = pd.DataFrame(bboxes_list, columns=['filename', 'x0', 'y0', 'x1', 'y1', 
         'x2', 'y2', 'x3', 'y3', 'text'])
         dataframe = dataframe.drop(columns=['x1', 'y1', 'x3', 'y3'])
 
@@ -41,6 +45,53 @@ def read_entities(path: Path):
     dataframe = pd.DataFrame([data])
     return dataframe
 
-ent_path = sroie_path / "test/entities/" / "X00016469670.txt"
+# combines bbox-file and entities-file into one DataFrame
+def assign_labels(bboxes: pd.DataFrame, entities: pd.DataFrame):
+    bboxes = bboxes.copy()
+    # define the length of labels and set all label to "O" initially
+    labels = ["O"] * len(bboxes)
+
+    row = entities.iloc[0]
+    company = row["company"]
+    address = row["address"]
+    date = row["date"]
+    total = row["total"]
+
+    total_found = False
+
+    for i, row in bboxes.iterrows():
+        line = row["text"]
+        y0 = row["y0"]
+
+        # 1. safe-check if ENTITY exists in entities
+        # 2. real-check if line is a subset of ENTITY
+
+        # line length at least 3, because according to this logic individual words like "MARKET" that appear in the COMPANY, have been classified as COMPANY which is wrong
+        # "line in company" and not "company in line" because company is sometimes separeted into multiple lines
+        if company and (line in company) and len(line) > 2:
+            labels[i] = "COMPANY"
+        # line length at least 3, because according to this logic individual numbers like "1" that appear in the ADDRESS, have been classified as ADDRESS which is wrong
+        # "line in address" and not "address in line" because address is mostly separeted into multiple lines
+        if address and (line in address) and len(line) > 2: 
+            labels[i] = "ADDRESS"
+        if date and (date in line):
+            labels[i] = "DATE"
+        if total and (total in line) and total_found == False:
+            # total value might occur more than only once
+            # always save the total value which has the highest y-position (so it is always the first one)
+            labels[i] = "TOTAL"
+            total_found = True
+
+    bboxes["label"] = labels
+    return bboxes
+
+
+# JUST FOR TEST
+bboxes_path = sroie_path / "train/box/" / "X51005663297.txt"
+bboxes = read_bboxes(bboxes_path)
+ent_path = sroie_path / "train/entities/" / "X51005663297.txt"
 entities = read_entities(ent_path)
-print(entities)
+
+dataset = assign_labels(bboxes, entities)
+print(dataset)
+
