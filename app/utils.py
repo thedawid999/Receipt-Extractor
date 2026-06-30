4595e66659c24500f8b39d84e11a6df1718e8423
thedawid999 <dawid.jedlinski@iu-study.org>
2026-06-30 15:11:03 +0200

    defined merge_bio_tags()

--- app/utils.py
+++ app/utils.py
@@ -17,31 +17,24 @@ def save_upload(file: UploadFile, folder="uploads"):
     
     return str(file_path)
 
-# converts ocr-bbox-format [[x1,y1], [x2,y2], [x3,y3], [x4,y4]] to annotation-format [x_min, y_min, x_max, y_max]
-def convert_to_topleft_and_bottomright(bbox: list[list[float, float]]):
-    x_coords = [i[0] for i in bbox]
-    y_coords = [i[1] for i in bbox]
+# combines same entities into one (e.g. B-COMPANY and I-COMPANY)
+def merge_bio_tags(words, labels):
+    entities = {}
+    current = None
 
-    x_min = int(min(x_coords))
-    y_min = int(min(y_coords))
-    x_max = int(max(x_coords))
-    y_max = int(max(y_coords))
+    for word, label in zip(words, labels):
 
-    return [x_min, y_min, x_max, y_max]
+        if label == "O":
+            current = None
+            continue
 
-# standardizes the bbox coordinates between 0-1000 for LayoutLMv3
-def standardize_bbox(bbox, height, width):
-    x_min, y_min, x_max, y_max = bbox
+        prefix, entity = label.split("-", 1)
 
-    x_min_n = int((x_min / width) * 1000)
-    y_min_n = int((y_min / height) * 1000)
-    x_max_n = int((x_max / width) * 1000)
-    y_max_n = int((y_max / height) * 1000)   
+        if prefix == "B":
+            entities.setdefault(entity, []).append(word)
+            current = entity
 
-    # to make sure that the coordinates stay between 0-1000 (ocr can create false coordinates like -2 if near to the edge)
-    return [
-        min(max(x_min_n, 0), 1000),
-        min(max(y_min_n, 0), 1000),
-        min(max(x_max_n, 0), 1000),
-        min(max(y_max_n, 0), 1000)
-    ]
+        elif prefix == "I" and current == entity:
+            entities[entity][-1] += " " + word
+
+    return entities
