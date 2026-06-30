ec1245e65d5ae1f88cbfd565684662e91d315923
thedawid999 <dawid.jedlinski@iu-study.org>
2026-06-30 15:58:53 +0200

    changed input for predict() to a single file instead of a list of files

--- app/pipeline_processor.py
+++ app/pipeline_processor.py
@@ -21,73 +21,65 @@ def process_input(path):
             return path
         else:
             raise ValueError("File must be of type .jpg, .jpeg or .png")
-    # if multiple files
+    # if path is a dir
     elif path.is_dir():
-        results = []
-        # get a full path of each file
-        for file_path in path.iterdir():
-            # check if created path is a file and if it's an image
-            if file_path.is_file() and file_path.suffix.lower() in {".jpg", ".jpeg", ".png"}:
-                results.append(file_path)
-        return results
-    else:
-        raise ValueError("Input must be a directory or a file of type .jpg, .jpeg or .png")
+        ValueError("Input must be a directory or a file of type .jpg, .jpeg or .png")
 
 # preprocess
 # OCR
 # LayoutLM predicition
-def predict(paths: list[Path]):
-    for path in paths:
-        img = Image.open(path) # loading the image
-        preprocessed = preprocess(path) # preprocessing the image
-        ocr_results = detect_text(preprocessed) # finding text
+def predict(path):
+    img = Image.open(path) # loading the image
+    preprocessed = preprocess(path) # preprocessing the image
+    ocr_results = detect_text(preprocessed) # finding text
 
-        text = [item['text'] for item in ocr_results]
-        boxes = [item['bbox'] for item in ocr_results]
+    text = [item['text'] for item in ocr_results]
+    boxes = [item['bbox'] for item in ocr_results]
 
-        # setting up the processor
-        encoding = processor(
-            images=img,
-            text=text,
-            boxes=boxes,
-            return_tensors="pt",
-            truncation=True,
-            padding="max_length"
-        )
+    # setting up the processor
+    encoding = processor(
+        images=img,
+        text=text,
+        boxes=boxes,
+        return_tensors="pt",
+        truncation=True,
+        padding="max_length"
+    )
 
-        # save ids to put tokens with the same id into one word (e.g. "ĠSD" and "N" --> "SDN")
-        word_ids = encoding.word_ids(batch_index=0)
+    # save ids to put tokens with the same id into one word (e.g. "ĠSD" and "N" --> "SDN")
+    word_ids = encoding.word_ids(batch_index=0)
 
-        # make the prediction
-        with torch.no_grad():
-            outputs = model(**encoding)
+    # make the prediction
+    with torch.no_grad():
+        outputs = model(**encoding)
 
-        predictions = outputs.logits.argmax(-1).squeeze().tolist()
-        word_predictions = {}
+    predictions = outputs.logits.argmax(-1).squeeze().tolist()
+    word_predictions = {}
 
-        # assign prediction for each word (using word_ids here to put tokens with same label into one word)
-        for token_idx, word_idx in enumerate(word_ids):
-            if word_idx is None:
-                continue
+    # assign prediction for each word (using word_ids here to put tokens with same label into one word)
+    for token_idx, word_idx in enumerate(word_ids):
+        if word_idx is None:
+            continue
 
-            if word_idx not in word_predictions:
-                word_predictions[word_idx] = predictions[token_idx]
+        if word_idx not in word_predictions:
+            word_predictions[word_idx] = predictions[token_idx]
 
-        # converting label_id into a string
-        words = []
-        labels = []
-        print(f"============== Predictions for image: {path} ==============")
-        for word_idx, label_id in word_predictions.items():
-            words.append(text[word_idx])
-            labels.append(model.config.id2label[label_id])
+    # converting label_id into a string
+    words = []
+    labels = []
+    print(f"============== Predictions for image: {path} ==============")
+    for word_idx, label_id in word_predictions.items():
+        words.append(text[word_idx])
+        labels.append(model.config.id2label[label_id])
 
-        # merging same bio tags
-        entities = merge_bio_tags(words, labels)
+    # merging same bio tags
+    entities = merge_bio_tags(words, labels)
         
-        # extracting only DATE in following formats dd.mm.yyy OR dd/mm/yyyy
-        entities["DATE"] = extract_date(entities["DATE"])
+    # extracting only DATE in following formats dd.mm.yyy OR dd/mm/yyyy
+    if "DATE" in entities:
+        entities["DATE"] = [extract_date(x) for x in entities["DATE"]]
 
-        return entities
+    return entities
 
 
 user_input = process_input("./samples/")
