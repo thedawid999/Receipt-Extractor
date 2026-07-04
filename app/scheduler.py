f3c9cb2b0077fe5f58e1d11dbf7679785dc2cd5c
thedawid999 <dawid.jedlinski@iu-study.org>
2026-07-04 09:16:35 +0200

    defined start_scheduler_cli() and start_scheduler_api()

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,18 +1,20 @@
-from apscheduler.schedulers.background import BackgroundScheduler, BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from .layoutlm import predict
 from .utils import save_to_file, resolve_image_paths, load_config
 from pathlib import Path
 import os
 
 api_mode = os.getenv("API_MODE", "false").lower() == "true"
-scheduler = None
+scheduler = BackgroundScheduler()
 
 def start_scheduler(config):
     if api_mode:
-        scheduler = BackgroundScheduler()
+        start_scheduler_api(config)
     else:
-        scheduler = BlockingScheduler()
+        start_scheduler_cli(config)
 
+# cli version contains infinite loop, which cannot be inside of api version
+def start_scheduler_cli(config):
     scheduler.add_job(
         daily_job,
         trigger="cron",
@@ -23,10 +25,26 @@ def start_scheduler(config):
 
     print(f"Scheduler started ({config['schedule']['hour']:02d}:{config['schedule']['minute']:02d})")
     scheduler.start()
-    
-    
-def stop_scheduler():
-    scheduler.shutdown()
+
+    try:
+        while True:
+            time.sleep(1)   
+    except KeyboardInterrupt:
+        print("Stopping scheduler...")
+        scheduler.shutdown()
+
+# api version runs without the infinite loop
+def start_scheduler_api(config):
+    scheduler.add_job(
+        daily_job,
+        trigger="cron",
+        hour=config["schedule"]["hour"],
+        minute=config["schedule"]["minute"],
+        args=[config]
+    )
+
+    print(f"Scheduler started ({config['schedule']['hour']:02d}:{config['schedule']['minute']:02d})")
+    scheduler.start()
 
 # contains every step that will be done once the scheduler starts
 def daily_job(config):
@@ -42,6 +60,9 @@ def daily_job(config):
     output_dir.mkdir(exist_ok=True)
     save_to_file(output_dir / output_file, results)
     print("Daily job finished!")
+
+def stop_scheduler():
+    scheduler.shutdown()
 
 
 
